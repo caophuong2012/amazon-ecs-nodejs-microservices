a4ae779ecc7f6b645f66cb8f517bcc4ee2c8895f
Phuong Cao <caophuong2012@gmail.com>
2022-05-25 21:51:52 +0700

    Fix path
    Signed-off-by: Phuong Cao <caophuong2012@gmail.com>

--- 3-microservices/deploy-api-v2-identity-migrate.sh
+++ 3-microservices/deploy-api-v2-identity-migrate.sh
@@ -77,7 +77,7 @@ do
 
 	# Build the container, and assign a tag to it for versioning
 	# (cd services/$SERVICE_NAME && npm install);
-	docker build -t identity-$SERVICE_NAME -f ./src/core/migrations/build/Dockerfile-migrations ./src/core/identity/migrations
+	docker build -t identity-$SERVICE_NAME -f ./src/core/identity/build/Dockerfile-migrations ./src/core/identity/migrations
 	docker tag identity-$SERVICE_NAME:latest $REPO:$TAG
 	# Push the tag up so we can make a task definition for deploying it
 	printf "${PRIMARY}* Pushing \`${SERVICE_NAME}\`${NC}\n";
